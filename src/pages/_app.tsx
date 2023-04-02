0609113f2d43e9750819b43ce173b2e2a529d1a6
Sven <svenrisse0@gmail.com>
2023-04-02 14:16:55 +0200

    switch to auth.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,19 @@
 import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import Head from "next/head";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Head>
-        <title>Migraine-Tracker</title>
-        <meta name="description" content="💭" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </ClerkProvider>
+    </SessionProvider>
   );
 };
 
