640a29fff732d8ca062c00b7598c7d4952c0f26a
Sven <svenrisse0@gmail.com>
2023-04-02 12:15:01 +0200

    feat(Clerk): set up clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,20 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Migraine-Tracker</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
