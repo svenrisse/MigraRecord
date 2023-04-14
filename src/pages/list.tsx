1737a1fbd3dd89cdb0c592bf3f418b1d951f8de2
Sven <svenrisse0@gmail.com>
2023-04-14 19:09:43 +0200

    list events to test data

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,21 +1,31 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Navbar from "~/components/Navbar";
+import { api } from "../utils/api";
 
 export default function List() {
   const router = useRouter();
-  const { data } = useSession();
+  const { data: authData } = useSession();
 
-  if (!data && typeof window !== "undefined") {
+  if (!authData && typeof window !== "undefined") {
     void router.push("/");
   }
+
+  const { data } = api.event.listEvents.useQuery();
+
+  const events = data?.map((event) => {
+    return (
+      <>
+        <div>this is a event</div>
+        <div>{event.startTime.toLocaleDateString()}</div>
+      </>
+    );
+  });
 
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#059669] to-[#115e59]">
-        <div>
-          <div>this is the list page</div>
-        </div>
+        <div>{events}</div>
       </main>
       <Navbar focused="list" />
     </>
