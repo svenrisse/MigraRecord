2f045485e0c35bb70d76d02508db60a0a94a51a7
Sven <svenrisse0@gmail.com>
2023-04-16 12:28:05 +0200

    style(Navbar): add basic styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,35 +9,35 @@ import { MdSpaceDashboard } from "react-icons/md";
 
 export default function Navbar({ focused }: { focused?: string }) {
   return (
-    <div className="fixed bottom-0 flex w-screen justify-evenly">
+    <div className="fixed bottom-0 flex w-screen justify-evenly rounded-t-lg bg-slate-300 pt-1">
       <Link href={"/dashboard"}>
         <MdSpaceDashboard
           size={"3rem"}
-          className={focused == "dashboard" ? "fill-green-300" : ""}
+          className={focused == "dashboard" ? "fill-emerald-700" : ""}
         />
       </Link>
       <Link href={"/addevent"}>
         <AiFillPlusCircle
           size={"3rem"}
-          className={focused == "addevent" ? "fill-green-300" : ""}
+          className={focused == "addevent" ? "fill-emerald-700" : ""}
         />
       </Link>
       <Link href={"/list"}>
         <AiOutlineUnorderedList
           size={"3rem"}
-          className={focused == "list" ? "fill-green-300" : ""}
+          className={focused == "list" ? "fill-emerald-700" : ""}
         />
       </Link>
       <Link href={"/calender"}>
         <AiFillCalendar
           size={"3rem"}
-          className={focused == "calender" ? "fill-green-300" : ""}
+          className={focused == "calender" ? "fill-emerald-700" : ""}
         />
       </Link>
       <Link href={"/settings"}>
         <AiFillSetting
           size={"3rem"}
-          className={focused == "settings" ? "fill-green-300" : ""}
+          className={focused == "settings" ? "fill-emerald-700" : ""}
         />
       </Link>
     </div>
