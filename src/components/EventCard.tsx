95a52737c562567d837ed7ffe6a283d812864c38
Sven <svenrisse0@gmail.com>
2023-04-16 11:39:34 +0200

    feat(EventCard): add basic info

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -6,8 +6,18 @@ export default function EventCard({
   event: RouterOutputs["event"]["getEvent"];
 }) {
   return (
-    <div className="my-2 w-11/12 rounded-md bg-slate-200 p-2 ">
-      <div>{event?.startTime.getDate()}</div>
+    <div className="my-2 rounded-md bg-slate-200 p-2 ">
+      <div className="flex gap-1">
+        <div>{event?.startTime.toLocaleDateString()}</div>
+        <div> - </div>
+        <div>
+          {event?.endTime ? (
+            <div>{event?.endTime?.toLocaleDateString()}</div>
+          ) : (
+            <div className="text-gray-500">No End Time yet</div>
+          )}
+        </div>
+      </div>
       <div className="flex gap-2">
         <h4>Type:</h4>
         {event?.type ? (
@@ -20,6 +30,14 @@ export default function EventCard({
         <h4>Pain:</h4>
         {event?.painScale ? (
           <div>{event.painScale}</div>
+        ) : (
+          <div className="text-gray-500">None</div>
+        )}
+      </div>
+      <div className="flex gap-2">
+        <h4>Notes:</h4>
+        {event?.notes ? (
+          <p>{event.notes}</p>
         ) : (
           <div className="text-gray-500">None</div>
         )}
