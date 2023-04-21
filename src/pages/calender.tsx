c4e8356cb77a014646eacbd178e81eea18f6be43
Sven <svenrisse0@gmail.com>
2023-04-21 12:28:01 +0200

    feat(calender): add auth validation

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -1,6 +1,15 @@
-import Navbar from "../components/Navbar";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Navbar from "~/components/Navbar";
 
 export default function Calender() {
+  const router = useRouter();
+  const { data: authData, status } = useSession();
+
+  if (!authData && typeof window !== "undefined" && status !== "loading") {
+    void router.push("/");
+  }
+
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0ea5e9] to-[#0e7490]">
