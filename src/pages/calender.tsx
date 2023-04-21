254801af57ef0737c482392c149ff0512e125aa4
Sven <svenrisse0@gmail.com>
2023-04-21 16:42:19 +0200

    feat(calender): render calender without data

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -1,6 +1,7 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Navbar from "~/components/Navbar";
+import Calendar from "react-calendar";
 
 export default function Calender() {
   const router = useRouter();
@@ -13,9 +14,8 @@ export default function Calender() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0ea5e9] to-[#0e7490]">
-        <div>
-          <div>this is the calender</div>
-          <p>more coming soon :)</p>
+        <div className="w-11/12 rounded-lg bg-slate-200 px-2 py-4">
+          <Calendar />
         </div>
       </main>
       <Navbar focused="calender" />
