0006b1d8b14c9537bf6647d024695d6550be25f6
Sven <svenrisse0@gmail.com>
2023-04-21 17:02:34 +0200

    feat(calender): highlight dates with startdate

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -2,8 +2,20 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Navbar from "~/components/Navbar";
 import Calendar from "react-calendar";
+import { isSameDay } from "date-fns";
+import { api } from "../utils/api";
 
 export default function Calender() {
+  function tileClassName({ date, view }) {
+    if (
+      dates &&
+      view === "month" &&
+      dates.find((dDate) => isSameDay(dDate, date))
+    ) {
+      return "bg-red-500";
+    }
+  }
+
   const router = useRouter();
   const { data: authData, status } = useSession();
 
@@ -11,11 +23,17 @@ export default function Calender() {
     void router.push("/");
   }
 
+  const { data } = api.event.listEvents.useQuery();
+
+  const dates = data?.map((date) => {
+    return date.startTime;
+  });
+
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0ea5e9] to-[#0e7490]">
         <div className="w-11/12 rounded-lg bg-slate-200 px-2 py-4">
-          <Calendar />
+          <Calendar tileClassName={tileClassName} />
         </div>
       </main>
       <Navbar focused="calender" />
