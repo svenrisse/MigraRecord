63e38949eaad43d902677f867daf64b794efa735
Sven <svenrisse0@gmail.com>
2023-04-22 12:50:44 +0200

    feat(calender): styling for first and last dates

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -7,29 +7,6 @@ import { api } from "../utils/api";
 import type { View } from "react-calendar/dist/cjs/shared/types";
 
 export default function Calender() {
-  function tileClassName({ date, view }: { view: View; date: Date }) {
-    if (
-      dates &&
-      firstDates &&
-      view == "month" &&
-      firstDates.find((dDate) => isSameDay(dDate as Date, date))
-    ) {
-      return "bg-red-500";
-    }
-
-    if (
-      dates &&
-      view === "month" &&
-      dates.flat().find((dDate) => isSameDay(dDate, date))
-    ) {
-      return "bg-cyan-700";
-    }
-  }
-
-  function handleDayClick(value: Date) {
-    console.log(value);
-  }
-
   const router = useRouter();
   const { data: authData, status } = useSession();
 
@@ -51,10 +28,62 @@ export default function Calender() {
         });
   });
 
-  const firstDates = dates?.map((d) => {
-    return d[0];
+  const sameDates = dates?.map((datePair) => {
+    if (datePair.length === 1) {
+      return datePair;
+    }
   });
 
+  const lastDates = dates?.map((datePair) => {
+    if (datePair.length > 1) {
+      return datePair[datePair.length - 1];
+    }
+  });
+
+  const firstDates = dates?.map((datePair) => {
+    return datePair[0];
+  });
+
+  function tileClassName({ date, view }: { view: View; date: Date }) {
+    if (
+      dates &&
+      sameDates &&
+      view == "month" &&
+      sameDates.flat().find((dDate) => isSameDay(dDate as Date, date))
+    ) {
+      return "bg-cyan-700 rounded-xl";
+    }
+
+    if (
+      dates &&
+      lastDates &&
+      view == "month" &&
+      lastDates.flat().find((dDate) => isSameDay(dDate as Date, date))
+    ) {
+      return "bg-cyan-700 rounded-r-xl";
+    }
+
+    if (
+      dates &&
+      firstDates &&
+      view == "month" &&
+      firstDates.find((dDate) => isSameDay(dDate as Date, date))
+    ) {
+      return "bg-cyan-700 rounded-l-xl";
+    }
+
+    if (
+      dates &&
+      view === "month" &&
+      dates.flat().find((dDate) => isSameDay(dDate, date))
+    ) {
+      return "bg-cyan-700";
+    }
+  }
+
+  function handleDayClick(value: Date) {
+    console.log(value);
+  }
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0ea5e9] to-[#0e7490]">
