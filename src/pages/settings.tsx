9773bc307112e481978ddeb505193781355fe83b
Sven <svenrisse0@gmail.com>
2023-05-10 20:15:42 +0200

    feat(settings): add buttons

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -13,11 +13,19 @@ export default function Settings() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0ea5e9] to-[#0e7490]">
-        <div>
-          <div>this is the settings page</div>
-          <p>more coming soon :)</p>
-
-          <button onClick={() => void signOut()}>Logout</button>
+        <div className="flex w-11/12 flex-col items-center rounded-lg bg-gray-50 px-2 py-4">
+          <button className="rounded-xl border-2 bg-cyan-600 px-4 py-2 font-bold text-white">
+            Edit Medications
+          </button>
+          <button className="rounded-xl border-2 bg-cyan-600 px-4 py-2 font-bold text-white">
+            Edit Questions
+          </button>
+          <button
+            className="rounded-xl border-2 bg-red-400 px-4 py-2 font-bold text-white"
+            onClick={() => void signOut()}
+          >
+            Logout
+          </button>
         </div>
       </main>
       <Navbar focused="settings" />
