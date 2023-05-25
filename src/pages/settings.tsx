4ac31b025ca0fcbb305adb9c6a61fba19072231f
Sven <svenrisse0@gmail.com>
2023-05-25 12:21:58 +0200

    feat(settings): add loading spinner and disable button

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -113,7 +113,7 @@ export default function Settings() {
             ) : (
               <div>{userMedications}</div>
             )}
-            <div className="flex gap-4 px-2">
+            <div className="flex h-10 gap-4 px-2">
               <input
                 type="text"
                 className="w-10/12 rounded-lg text-center"
@@ -122,10 +122,34 @@ export default function Settings() {
                 min={1}
               ></input>
               <button
-                className="rounded-xl bg-cyan-600 px-4 py-2"
+                className="w-14 rounded-xl bg-cyan-600 px-4 py-2"
                 type="submit"
+                disabled={questionIsLoading || medicationIsLoading}
               >
-                +
+                {medicationIsLoading || questionIsLoading ? (
+                  <svg
+                    className="h-5 w-5 animate-spin text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      stroke-width="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                ) : (
+                  <div>+</div>
+                )}
               </button>
             </div>
           </form>
