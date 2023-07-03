15686e5b098975050ba6f3d6880295cc25cfbdf2
Sven <svenrisse0@gmail.com>
2023-07-03 10:55:40 +0200

    refactor(ActiveRange): move logic from active Button to component

--- src/components/ActiveRange.tsx
+++ src/components/ActiveRange.tsx
@@ -1,61 +1,66 @@
 import { subMonths } from "date-fns";
+import { useState } from "react";
 
 export default function ActiveRange({
-  activeRange,
   setActiveRange,
 }: {
-  activeRange: { all: boolean; range: number; limit: Date };
   setActiveRange: React.Dispatch<React.SetStateAction<any>>;
 }) {
+  const [active, setActive] = useState("all");
   return (
     <>
       <div className="mt-6 flex gap-4 rounded-xl bg-gray-50 px-2 py-2">
         <button
-          className={`${activeRange.range === 1 && "bg-cyan-600  text-white"
-            } rounded-xl  border-2 border-cyan-600 px-4 py-2 font-bold`}
-          onClick={() =>
+          className={`${
+            active === "1" && "bg-cyan-600  text-white"
+          } rounded-xl  border-2 border-cyan-600 px-4 py-2 font-bold`}
+          onClick={() => {
+            setActive("1");
             setActiveRange({
               all: false,
-              range: 1,
               limit: subMonths(new Date(), 1),
-            })
-          }
+            });
+          }}
         >
           1M
         </button>
 
         <button
-          className={`${activeRange.range === 3 && "bg-cyan-600 text-white"
-            } rounded-xl  border-2 border-cyan-600 px-4 py-2 font-bold`}
-          onClick={() =>
+          className={`${
+            active === "3" && "bg-cyan-600 text-white"
+          } rounded-xl  border-2 border-cyan-600 px-4 py-2 font-bold`}
+          onClick={() => {
+            setActive("3");
             setActiveRange({
               all: false,
-              range: 3,
               limit: subMonths(new Date(), 3),
-            })
-          }
+            });
+          }}
         >
           3M
         </button>
         <button
-          className={`${activeRange.range === 6 && "bg-cyan-600  text-white"
-            } rounded-xl  border-2 border-cyan-600 px-4 py-2 font-bold`}
-          onClick={() =>
+          className={`${
+            active === "6" && "bg-cyan-600  text-white"
+          } rounded-xl  border-2 border-cyan-600 px-4 py-2 font-bold`}
+          onClick={() => {
+            setActive("6");
             setActiveRange({
               all: false,
-              range: 6,
               limit: subMonths(new Date(), 6),
-            })
-          }
+            });
+          }}
         >
           6M
         </button>
         <button
-          className={`${activeRange.all === true && "bg-cyan-600  text-white"
-            } rounded-xl  border-2 border-cyan-600 px-4 py-2 font-bold`}
-          onClick={() =>
-            setActiveRange({ all: true, range: 0, limit: new Date() })
-          }
+          className={`${
+            active === "all" && "bg-cyan-600  text-white"
+          } rounded-xl  border-2 border-cyan-600 px-4 py-2 font-bold`}
+          onClick={() => {
+            setActive("all");
+            setActiveRange({ all: true, limit: new Date() });
+          }}
         >
           All
         </button>
