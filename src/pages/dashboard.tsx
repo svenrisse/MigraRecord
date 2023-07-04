1c3bc98c336c669f26d8425df95806ab292a2fe3
Sven <svenrisse0@gmail.com>
2023-07-04 08:55:07 +0200

    feat(dashboard): add active range to dashboard with api

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,10 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Navbar from "~/components/Navbar";
+import { api } from "../utils/api";
+import { useState } from "react";
+import ActiveRange from "~/components/ActiveRange";
+import { TailSpin } from "react-loader-spinner";
 
 export default function Dashboard() {
   const router = useRouter();
@@ -9,14 +13,32 @@ export default function Dashboard() {
   if (!authData && typeof window !== "undefined" && status !== "loading") {
     void router.push("/");
   }
+  const [activeRange, setActiveRange] = useState({
+    all: true,
+    limit: new Date(),
+  });
+
+  const { data, isFetching } = api.event.listEventsInRange.useQuery({
+    all: activeRange.all,
+    limit: activeRange.limit,
+  });
 
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0ea5e9] to-[#0e7490]">
-        <div>
-          <div>this is the dashboard</div>
-          <p>Stats coming soon :)</p>
+        <div className="absolute top-0">
+          <ActiveRange setActiveRange={setActiveRange} />
         </div>
+        {isFetching ? (
+          <div className="rounded-lg bg-slate-200 px-8 py-6">
+            <TailSpin color="cyan" />
+            <p className="">Loading...</p>
+          </div>
+        ) : (
+          <div className="flex w-9/12 flex-col gap-3 pb-20 pt-32">
+            dashboard
+          </div>
+        )}
       </main>
       <Navbar focused="dashboard" />
     </>
