aae344478c85dd5fb59d1df06587e90ad9d86da8
Sven <svenrisse0@gmail.com>
2023-07-10 18:41:32 +0200

    feat(QuestionModal): finish form

--- src/components/QuestionModal.tsx
+++ src/components/QuestionModal.tsx
@@ -29,41 +29,66 @@ export default function QuestionModal({
     console.log(data);
   };
 
+  const watchAmount = watch("amount");
+
   return (
     <Modal
       isOpen={modalIsOpen}
       onRequestClose={closeModal}
-      className="fixed inset-x-0 top-1/2 mx-auto flex w-2/3 flex-col items-center rounded-lg border-0 bg-slate-300 py-8 md:w-5/12 lg:w-1/4 lg:py-12 xl:w-1/5 2xl:w-1/6"
+      className="fixed inset-x-0 top-1/2 mx-auto w-10/12 rounded-lg border-0 bg-slate-300 py-8 md:w-5/12 lg:w-1/4 lg:py-12 xl:w-1/5 2xl:w-1/6"
       appElement={document.getElementById("__next") as HTMLElement}
     >
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col items-center gap-2 font-sans"
+        className="flex flex-col gap-2 font-sans"
       >
-        <select {...register("name")} name="medications" required>
-          {medicationOptions}
-        </select>
-        <div>
-          <button type="button">+</button>
+        <div className="flex flex-col items-center gap-4">
+          <select
+            {...register("name")}
+            name="medications"
+            required
+            className="w-min rounded-md border-2 border-cyan-900 bg-white p-1"
+          >
+            {medicationOptions}
+          </select>
+          <div className="flex font-bold">
+            <button
+              type="button"
+              className="w-8 rounded-l-lg bg-gray-200"
+              onClick={() => setValue("amount", watchAmount + 1)}
+            >
+              +
+            </button>
 
+            <input
+              {...register("amount")}
+              type="number"
+              defaultValue={0}
+              min={1}
+              className="w-8 py-1 text-center [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+              required
+            />
+            <button
+              type="button"
+              className="w-8 rounded-r-lg bg-gray-200"
+              onClick={() => setValue("amount", watchAmount - 1)}
+            >
+              -
+            </button>
+          </div>
           <input
-            {...register("amount")}
-            type="number"
-            placeholder="amount"
-            defaultValue={0}
-            min={1}
-            className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+            type="datetime-local"
+            {...register("date")}
+            className="rounded-md border-2 border-cyan-900 bg-white p-1"
             required
           />
-          <button type="button">-</button>
+          <button
+            type="submit"
+            className="rounded-xl bg-cyan-600 px-4 py-2 font-bold text-white"
+          >
+            Save
+          </button>
         </div>
-        <input
-          type="datetime-local"
-          {...register("date")}
-          className="rounded-md border-2 border-cyan-900 bg-white p-1"
-          required
-        />
-        <button type="submit">Save</button>
       </form>
     </Modal>
   );
