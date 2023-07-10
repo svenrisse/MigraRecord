56e92a808d1f826a0b881cd35de360310f952436
Sven <svenrisse0@gmail.com>
2023-07-10 16:56:42 +0200

    feat(QuestionModal): add missing things for form

--- src/components/QuestionModal.tsx
+++ src/components/QuestionModal.tsx
@@ -1,7 +1,8 @@
 import Modal from "react-modal";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { object, string, array, coerce, number, z } from "zod";
+import { object, string, coerce, number, z } from "zod";
+import type { SubmitHandler } from "react-hook-form";
 
 export const questionSchema = object({
   name: string(),
@@ -20,9 +21,13 @@ export default function QuestionModal({
   closeModal: () => void;
   medicationOptions: JSX.Element[] | undefined;
 }) {
-  const { register, handleSubmit, control, setValue, watch } = useForm<Inputs>({
+  const { register, handleSubmit, setValue, watch } = useForm<Inputs>({
     resolver: zodResolver(questionSchema),
   });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
 
   return (
     <Modal
@@ -31,27 +36,32 @@ export default function QuestionModal({
       className="fixed inset-x-0 top-1/2 mx-auto flex w-2/3 flex-col items-center rounded-lg border-0 bg-slate-300 py-8 md:w-5/12 lg:w-1/4 lg:py-12 xl:w-1/5 2xl:w-1/6"
       appElement={document.getElementById("__next") as HTMLElement}
     >
-      <select {...register("name")} name="medications">
-        {medicationOptions}
-      </select>
-      <div>
-        <button>+</button>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center gap-2 font-sans"
+      >
+        <select {...register("name")} name="medications">
+          {medicationOptions}
+        </select>
+        <div>
+          <button>+</button>
 
+          <input
+            {...register("amount")}
+            type="number"
+            placeholder="amount"
+            defaultValue={0}
+            className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+          />
+          <button>+</button>
+        </div>
         <input
-          {...register("amount")}
-          type="number"
-          placeholder="amount"
-          defaultValue={0}
-          className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+          type="datetime-local"
+          {...register("date")}
+          className="rounded-md border-2 border-cyan-900 bg-white p-1"
+          required
         />
-        <button>+</button>
-      </div>
-      <input
-        type="datetime-local"
-        {...register("date")}
-        className="rounded-md border-2 border-cyan-900 bg-white p-1"
-        required
-      />
+      </form>
     </Modal>
   );
 }
