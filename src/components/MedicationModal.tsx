87b1f9f893698283c6ec2b0078c486b03d571841
Sven <svenrisse0@gmail.com>
2023-07-12 15:01:02 +0200

    feat(MedicationModal): add loading spinner and reset on mutate

--- src/components/MedicationModal.tsx
+++ src/components/MedicationModal.tsx
@@ -26,7 +26,7 @@ export default function MedicationModal({
   medicationOptions: JSX.Element[] | undefined;
   id: string;
 }) {
-  const { control, register, handleSubmit, setValue } = useForm<Inputs>({
+  const { control, reset, register, handleSubmit, setValue } = useForm<Inputs>({
     resolver: zodResolver(questionSchema),
   });
 
@@ -40,7 +40,12 @@ export default function MedicationModal({
     });
   };
 
-  const { mutateAsync } = api.eventMedication.addEventMedication.useMutation();
+  const { mutateAsync, isLoading } =
+    api.eventMedication.addEventMedication.useMutation({
+      onSuccess() {
+        reset();
+      },
+    });
 
   return (
     <Modal
@@ -82,7 +87,30 @@ export default function MedicationModal({
             type="submit"
             className="rounded-xl bg-cyan-600 px-4 py-2 font-bold text-white"
           >
-            Save
+            {!isLoading ? (
+              <svg
+                className="h-5 w-5 animate-spin text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            ) : (
+              <div>Save</div>
+            )}
           </button>
         </div>
       </form>
