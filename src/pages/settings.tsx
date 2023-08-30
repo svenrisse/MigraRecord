61a792b402f62c07ef7c1a7d92de8708396744b0
Sven <svenrisse0@gmail.com>
2023-08-30 14:35:38 +0200

    feat(settings): add new navbar

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -106,7 +106,7 @@ export default function Settings() {
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0ea5e9] to-[#0e7490]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-background">
         <div className="flex w-11/12 flex-col items-center gap-3 rounded-lg bg-gray-50 px-2 py-4">
           <button
             className="rounded-xl border-2 bg-cyan-600 px-4 py-2 font-bold text-white"
