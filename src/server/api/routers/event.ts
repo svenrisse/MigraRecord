97a73f0657b7c9dcb2ae2c9dbb737f9420704f60
Sven <svenrisse0@gmail.com>
2023-09-03 10:24:18 +0200

    fix(eventRouter): eventSchema import

--- src/server/api/routers/event.ts
+++ src/server/api/routers/event.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { eventFormSchema as eventSchema } from "~/types/types";
+import { eventSchema } from "../../../components/EventForm";
 import { z } from "zod";
 
 export const eventRouter = createTRPCRouter({
