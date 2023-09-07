7784dd7fd9d9ddf987866107f1d66d8d4882a82a
Sven <svenrisse0@gmail.com>
2023-09-07 15:52:43 +0200

    feat(EventForm): change loading spinner to daisy

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -357,29 +357,10 @@ export default function EventForm({ id }: { id?: string }) {
 
             <button
               type="submit"
-              className="mb-1 rounded-xl border-2 bg-cyan-600 px-4 py-2 font-bold text-white"
+              className="btn-primary btn my-2 font-bold text-white"
             >
               {isLoading ? (
-                <svg
-                  className="h-5 w-5 animate-spin text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    stroke-width="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
+                <span className="loading loading-spinner"></span>
               ) : (
                 <div>Save</div>
               )}
