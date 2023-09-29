d6e38ae1b76b3cba15a3c62ddc0c252427efb56d
Sven <svenrisse0@gmail.com>
2023-09-29 10:16:58 +0200

    build(tailwind): add screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ export default {
         background: "#002233",
       },
     },
+    screens: {
+      sm: "375px",
+      md: "800px",
+      lg: "1200px",
+      xl: "1600px",
+      "2xl": "2200px",
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
