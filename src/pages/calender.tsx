c62adf97468651ca3a112a28a23e3d0ab31de844
Sven <svenrisse0@gmail.com>
2023-09-30 08:33:06 +0200

    feat(calender): add responsive bg-images

--- src/pages/calender.tsx
+++ src/pages/calender.tsx
@@ -31,13 +31,13 @@ export default function Calender() {
   const dates = data?.map((event) => {
     return event.endTime
       ? eachDayOfInterval({
-          start: event.startTime,
-          end: event.endTime,
-        })
+        start: event.startTime,
+        end: event.endTime,
+      })
       : eachDayOfInterval({
-          start: event.startTime,
-          end: event.startTime,
-        });
+        start: event.startTime,
+        end: event.startTime,
+      });
   });
 
   const sameDates = dates?.map((datePair) => {
@@ -116,11 +116,10 @@ export default function Calender() {
   return (
     <>
       <main
-        className={`${
-          theme === "customlight"
-            ? "bg-[url('/blob-scene-white.svg')]"
-            : "bg-[url('/blob-scene.svg')]"
-        } flex min-h-screen flex-col items-center justify-center bg-fixed`}
+        className={`${theme === "customlight"
+            ? "bg-[url('/blob-scene-customlight-phone.svg')] md:bg-[url('/blob-scene-customlight-tablet.svg')] lg:bg-[url('/blob-scene-customlight-laptop.svg')] xl:bg-[url('/blob-scene-customlight-big.svg')] 2xl:bg-[url('/blob-scene-customlight-huge.svg')]"
+            : "bg-[url('/blob-scene-customdark-phone.svg')] md:bg-[url('/blob-scene-customdark-tablet.svg')] lg:bg-[url('/blob-scene-customdark-laptop.svg')] xl:bg-[url('/blob-scene-customdark-big.svg')] 2xl:bg-[url('/blob-scene-customdark-huge.svg')]"
+          } flex min-h-screen flex-col items-center justify-center bg-fixed`}
       >
         <div className="w-11/12 rounded-lg bg-base-100 px-2 py-4 shadow-2xl">
           {eventsIsLoading ? (
